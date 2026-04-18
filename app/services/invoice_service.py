6b84ea1d961ea073ae2769a5605ba50c2f3c4072
Sonya <preminina.sofya@gmail.com>
2026-04-18 23:45:41 +0300

    feat(invoice): add admin service method to fetch all invoices with pagination

--- app/services/invoice_service.py
+++ app/services/invoice_service.py
@@ -1,10 +1,17 @@
+from uuid import UUID
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.db.models.invoice import Invoice
 
 
 class InvoiceService:
+
     @staticmethod
-    async def get_invoices(db, user_id):
+    async def get_invoices(
+        db: AsyncSession, 
+        user_id: UUID
+    ):
         result = await db.execute(
             select(Invoice).where(
                 Invoice.user_id == user_id
@@ -13,8 +20,13 @@ class InvoiceService:
 
         return result.scalars().all()
     
+    
     @staticmethod
-    async def get_one_invoice(db, invoice_id, user_id):
+    async def get_one_invoice(
+        db: AsyncSession, 
+        invoice_id: int, 
+        user_id: int
+    ):
         result = await db.execute(
             select(Invoice).where(
                 Invoice.id == invoice_id,
@@ -23,3 +35,18 @@ class InvoiceService:
         )
 
         return result.scalar_one_or_none()
+
+
+    @staticmethod
+    async def get_all_invoices(
+        db: AsyncSession,
+        limit: int = 50,
+        offset: int = 0
+    ):
+        result = await db.execute(
+            select(Invoice)
+            .limit(limit)
+            .offset(offset)
+        )
+
+        return result.scalars().all()
